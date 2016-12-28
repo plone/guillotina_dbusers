3c82615f27c66159dd5b22fa3c9d8c27a04af967
vangheem <vangheem@gmail.com>
2016-12-28 11:16:43 -0600

    user info should provide more info

--- pserver/zodbusers/services/users.py
+++ pserver/zodbusers/services/users.py
@@ -1,14 +1,22 @@
 from plone.server.api.service import Service
 from plone.server.browser import Response
 from plone.server.utils import get_authenticated_user
+from plone.server.json.interfaces import IResourceSerializeToJson
+from zope.component import queryMultiAdapter
 
 
 class Info(Service):
 
     async def __call__(self):
         user = get_authenticated_user(self.request)
-        return Response({
+        serializer = queryMultiAdapter((user, self.request), IResourceSerializeToJson)
+        if serializer:
+            data = serializer()
+        else:
+            data = {}
+        data.update({
             'id': user.id,
             'roles': user._roles,
-            'groups': user._groups
+            'groups': getattr(user, '_groups', [])
         })
+        return Response(data)
